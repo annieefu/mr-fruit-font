1d0078fb54b077ef4e9c52971c5b22f17437e8ca
Annie Fu <anniefu@mac.mynetworksettings.com>
2025-06-22 02:01:47 -0400

    initial layout + gallery

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,8 +1,8 @@
 <script>
 	let {
-		title = "Title TK",
+		title = "The Mr. Fruit Font",
 		description = "Description TK",
-		url = "https://pudding.cool",
+		url = "",
 		keywords = "",
 		preloadFont = []
 	} = $props();
@@ -11,11 +11,11 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	<meta name="author" content="The Pudding" />
+	<meta name="author" content="Annie Fu" />
 	<meta name="news_keywords" content={keywords} />
 
 	<meta property="og:title" content={title} />
-	<meta property="og:site_name" content="The Pudding" />
+	<meta property="og:site_name" content="Mr. Fruit Font" />
 	<meta property="og:url" content={url} />
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="article" />
@@ -27,11 +27,11 @@
 	<meta property="og:image:height" content="628" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content="https://pudding.cool" />
-	<meta name="twitter:creator" content="@puddingviz" />
+	<meta name="twitter:site" content="" />
+	<meta name="twitter:creator" content="@annieefu" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image:src" content="{url}/assets/social-twitter.jpg" />
+	<meta name="twitter:image:src" content="" />
 
 	<meta name="robots" content="max-image-preview:large" />
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,114 @@
-<script>
-	import { setContext } from "svelte";
-	import { browser } from "$app/environment";
-	import Meta from "$components/Meta.svelte";
-	import Index from "$components/Index.svelte";
-	import copy from "$data/copy.json";
-	import version from "$utils/version.js";
+<div class="header">
+  <span class="top">The</span>
+  <span class="middle">Mr. Fruit</span>
+  <span class="bottom">Font</span>
+</div>
 
-	let { data } = $props();
+<div class="content">
+<p>The Mr. Fruit Font is an homage to the handwritten produce signs that populate the storefronts of Mr. Fruit bodegas across Brooklyn (Mr. Kiwi, Mr. Kale, Mr. Lemon, Mr. Mango, & more).
+</p>
+<span class="content">image gallery</span>
+</div>
 
-	version();
+<div class="gallery">
+  <img src="/imgs/IMG_0221.JPG" alt="Mr. Fruit Gallery Image 1">
+  <img src="/imgs/IMG_0220.JPG" alt="Mr. Fruit Gallery Image 3">
+  <img src="/imgs/IMG_0223.JPG" alt="Mr. Fruit Gallery Image 3">
+  <img src="/imgs/IMG_0222.JPG" alt="Mr. Fruit Gallery Image 2">
+  <img src="/imgs/IMG_0224.JPG" alt="Mr. Fruit Gallery Image 4">
+  <img src="/imgs/IMG_0214.JPG" alt="Mr. Fruit Gallery Image 3">
+  <img src="/imgs/IMG_0225.JPG" alt="Mr. Fruit Gallery Image 5">
+  <img src="/imgs/IMG_0226.JPG" alt="Mr. Fruit Gallery Image 6">
+  <img src="/imgs/IMG_0227.JPG" alt="Mr. Fruit Gallery Image 7">
+</div>
+<br/><br/>
+<style>
+@font-face {
+  font-family: "Mr Fruit";
+  src: url("/assets/MrFruit3-Regular.woff2") format("woff2");
+  font-display: swap;
+}
 
-	const preloadFont = [
-		"https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Regular.woff2",
-		"https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Bold.woff2",
-		"https://pudding.cool/assets/fonts/atlas/AtlasGrotesk-Regular-Web.woff2",
-		"https://pudding.cool/assets/fonts/atlas/AtlasGrotesk-Bold-Web.woff2"
-	];
+.header {
+  margin: auto;
+  position: relative;
+  height: 250px;
+  width: 90%;
+  max-width: 600px;
+}
 
-	const { title, description, url, keywords } = copy;
-	setContext("copy", copy);
-	setContext("data", data);
-</script>
 
-<Meta {title} {description} {url} {preloadFont} {keywords} />
-<Index />
+.content {
+  margin: auto;
+  position: relative;
+  height: 200px;
+  width: 90%;
+  max-width: 600px;
+}
+.gallery {
+  margin: auto;
+  position: relative;
+  width: 90%;
+  max-width: 1000px;
+  column-count: 3;
+  column-gap: 1rem;
+}
+
+.gallery img {
+  width: 100%;
+  margin-bottom: 1rem;
+  break-inside: avoid;
+  display: block;
+}
+
+
+span {
+  font-family: 'Mr Fruit', sans-serif;
+  font-size: 48px;
+  position: absolute;
+}
+
+p{
+	font-size: 20px;
+	font-weight: 600;
+	/* font-family: 'Mr Fruit', sans-serif; */
+}
+
+.top {
+  top: 0;
+  left: 0;
+  color: #df1b0d;
+}
+
+.middle {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 90px;
+  text-align: center;
+  white-space: nowrap;
+}
+
+.bottom {
+  bottom: 0;
+  right: 0;
+  color: #df1b0d;
+}
+
+/* 🔽 Media Queries for responsiveness */
+@media (max-width: 600px) {
+  .header {
+    width: 100%;
+    height: 220px; /* Optional: adjust if space is tight */
+  }
+
+  .middle {
+    font-size: 12vw; /* Scale with screen width */
+  }
+
+  .top,
+  .bottom {
+    font-size: 6vw;
+  }
+}
+</style>
